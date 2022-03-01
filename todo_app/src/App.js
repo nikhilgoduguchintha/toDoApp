b757a5a3f2c917ed8b0086e7ef9916fd0fcf7abb
Nikhil Goduguchintha <nikhilnikki747@gmail.com>
2022-03-01 13:34:53 +0530

    first commit

---
+++ todo_app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import TODOApp from './components';
+import './App.css';
+
+function App() {
+  return (
+    <div className='initial-style'>
+      <h1>Task List</h1>
+      <div>
+        <TODOApp />
+      </div>
+    </div>
+  );
+}
+
+export default App;
